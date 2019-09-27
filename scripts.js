6e9d2ae2f10de5d78fbfe963f11756fa2b51d0ec
Troy Rios <troy.rios@gmail.com>
2019-09-27 13:07:13 +0000

    Fix name of script and add loop to js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
 function randomNumber(upper) {
   return Math.floor( Math.random() * upper ) + 1;
 }
+var i = 0;
+while (i < 10 ) 
+{
+  var intRand = randomNumber(6);
+  document.write(intRand + ' ');
+  i += 1;
+}
